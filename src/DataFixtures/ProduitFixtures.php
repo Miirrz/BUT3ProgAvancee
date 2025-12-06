b663da5155e36fd95fa85daa31200056a448ca33
gatiendubot <gatiendubot@gmail.com>
2025-12-06 04:43:11 +0000

    Ajouts de fixtures + Début page d'accueil et bannière

--- src/DataFixtures/ProduitFixtures.php
+++ src/DataFixtures/ProduitFixtures.php
@@ -34,11 +34,28 @@ class ProduitFixtures extends Fixture
         $manager->flush();
     }
 
-    // "nom du produit => "Lien de l'Image"
     const produits = [
         "Fiole d'Estus" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/2005.png",
         "Dague" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Wpn_Dagger.png",
         "Lance" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/spear.png",
-        "Rondache" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/buckler.png"
+        "Rondache" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/buckler.png",
+        "Zweihander" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/zweihander.png",
+        "Skull Lantern" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/skull_lantern.png",
+        "Poing en os de dragon" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/dragon_bone_fist_1.png",
+        "Anneau d'Havel" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/4000.png",
+        "Masque du père" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/mask_of_the_father.png",
+        "Anneau de grâce protectrice" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/4029.png",
+        "Anneau de cloranthy" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/4004.png",
+        "Anneau de grâce protectrice" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/4029.png",
+        "Grand bouclier d'Artorias" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/greatshield_of_artorias_1.png",
+        "Claymore" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/claymore.png",
+        "Humanité" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/2112.png",
+        "Espadon du clair de lune" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/moonlight_greatsword.png",
+        "Casque du chevalier d'élite" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/elite_knight_helm.png",
+        "Armure du chevalier d'élite" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/elite_knight_armor.png",
+        "Gantelets du chevalier d'élite" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/elite_knight_gauntlets.png",
+        "Jambières du chevalier d'élite" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/elite_knight_leggings.png",
+        "Os du retour" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/2034.png",
+        "Touffe de mousse violette" => "https://darksouls.wiki.fextralife.com/file/Dark-Souls/2016.png"
     ];
 }
